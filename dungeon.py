e9ccfa059c772d780b1bcc0436ce847e7d504853
Milan H <spidermila@gmail.com>
2021-04-11 00:39:26 +0200

    test

--- dungeon.py
+++ dungeon.py
@@ -1,20 +1,23 @@
 from typing import List
 
+
 class Player:
     def __init__(self) -> None:
-        self.name = ""
+        self.name = ''
         self.items: List[Item] = []
+
 
 class Room:
     def __init__(self, id: int) -> None:
         self.id = id
         self.items: List[Item] = []
         self.connections: List[int] = []
-        self.description = ""
+        self.description = ''
 
     def connect_to(self, room_ids: list) -> None:
         for r in room_ids:
             self.connections.append(r)
+
 
 class Dungeon:
     def __init__(self) -> None:
@@ -24,15 +27,23 @@ class Dungeon:
         if len(self.rooms) == 0:
             self.rooms.append(Room(0))
 
+
 class Item:
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.type = ""
+    def __init__(self, properties: dict) -> None:
+        self.name = properties['name']
+        self.type = properties['type']
+        self.weight = properties['weight']
 
 
 def main():
     p = Player()
-    d = Dungeon()
+    p.items.append(
+        Item({
+            'name': 'Hat',
+            'type': 'clothes',
+            'weight': 1,
+        }),
+    )
 
 
 if __name__ == '__main__':
