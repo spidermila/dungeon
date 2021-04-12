5b9dc785703eeff894597d01f3333bc83f662154
spidermila <spidermila@gmail.com>
2021-04-13 00:48:26 +0200

    first interactive commands

--- dungeon.py
+++ dungeon.py
@@ -5,12 +5,16 @@ from typing import Dict
 from typing import List
 from typing import Union
 
+class World:
+    def __init__(self) -> None:
+        self.rooms: List[Room] = []
+
 class Room:
-    def __init__(self, id: int) -> None:
+    def __init__(self, id: int, description: str) -> None:
         self.id = id
         self.items: List[Item] = []
         self.connections: List[int] = []
-        self.description = ''
+        self.description = description
 
     def connect_to(self, room_ids: list) -> None:
         for r in room_ids:
@@ -45,7 +49,8 @@ class Weapon(Item):
         self.damage = damage
 
 class Player:
-    def __init__(self) -> None:
+    def __init__(self, room: Room) -> None:
+        self.room = room
         self.name: str = ''
         self.items: List[Item] = []
         self.HP: int = 30
@@ -88,7 +93,7 @@ class Player:
             raise AssertionError('Wrong position name')
 
     def show_wearing(self) -> None:
-        for k in self.wearable_positions_occupied.keys():
+        for k in self.wearable_positions_occupied:
             if self.wearable_positions_occupied[k]: # occupied position
                 print(f'{k}: {self.wearing[k].name}')
             else:
@@ -98,11 +103,49 @@ class Player:
         for i in self.items:
             print(i.name)
 
+    def look(self):
+        print(self.room.description)
+        if len(self.room.items) > 0:
+            print('Items in the room:')
+            for i in self.room.items:
+                print(i.name)
+        else:
+            print('There are no items in the room')
+
+
 def main():
-    p = Player()
+    world = World()
+    world.rooms.append(Room(id = 1, description = 'A small dark room'))
+    p = Player(room = world.rooms[0])
     p.items.append(Item(name = 'neco', weight = 2))
     p.items.append(Clothes(name = 'shirt', wearable_positions = ['body'], weight = 2, color = 'red'))
     p.items.append(Weapon(name = 'dagger', weight = 1, damage = 1))
+    p.room.items.append(Clothes(name = 'simple hat', wearable_positions = ['head'], weight = 1, color = 'gray'))
+
+    commands = {
+        'quit commands': ['q', 'quit', 'exit'],
+        'help commands': ['h', 'help'],
+        'character commands': ['c', 'char', 'character'],
+        'inventory commands': ['i', 'inv', 'inventory'],
+        'look commands': ['l', 'look'],
+    }
+
+    while True:
+        command = input(": ")
+        if command.lower() in commands['quit commands']:
+            break
+        elif command.lower() in commands['help commands']:
+            for c in commands:
+                print(f'{c}: {commands[c]}')
+        elif command.lower() in commands['character commands']:
+            p.show_wearing()
+        elif command.lower() in commands['inventory commands']:
+            p.show_inventory()
+        elif command.lower() in commands['look commands']:
+            p.look()
+        else:
+            print('Unknown command')
+'''\
     p.wear(source = p.items, item = p.items[1], position = 'body')
     p.show_inventory()
     p.show_wearing()
@@ -110,7 +153,7 @@ def main():
     p.strip(destination = p.items, position = 'body')
     p.show_inventory()
     p.show_wearing()
-
+'''
 
 if __name__ == '__main__':
     exit(main())
