1cd82c8a9d8a880ee259d52ffa4bc717a081e058
spidermila <spidermila@gmail.com>
2021-04-14 00:25:35 +0200

    equip method added

--- dungeon.py
+++ dungeon.py
@@ -25,14 +25,16 @@ class Item:
     def __init__(self, name: str, weight: int) -> None:
         self.name = name
         self.weight = weight
+        self.equippable = False
+        self.equippable_positions: list = []
 
 class Clothes(Item):
     type = 'clothes'
-    def __init__(self, color: str, wearable_positions: List[str], *args, **kwargs) -> None:
+    def __init__(self, color: str, equippable_positions: List[str], *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.color = color
-        self.wearable = True
-        self.wearable_positions = wearable_positions
+        self.equippable = True
+        self.equippable_positions = equippable_positions
 
 class Weapon(Item):
     type = 'weapon'
@@ -44,9 +46,9 @@ class Player:
     def __init__(self, room: Room) -> None:
         self.room = room
         self.name: str = ''
-        self.items: List[Item] = []
+        self.inventory: List[Item] = []
         self.HP: int = 30
-        self.wearable_positions_occupied: Dict[str, bool] = {
+        self.equippable_positions_occupied: Dict[str, bool] = {
             'head': False,
             'body': False,
             'left arm': False,
@@ -55,13 +57,13 @@ class Player:
             'legs': False,
             'feet': False,
         }
-        self.wearing = {} # type: ignore
+        self.wearing: dict = {}
 
-    def wear(self, source: list, item: Clothes, position: str) -> bool:
-        if position in self.wearable_positions_occupied:
-            if not self.wearable_positions_occupied[position]:
+    def equip(self, source: list, item: Item, position: str) -> bool:
+        if position in self.equippable_positions_occupied:
+            if not self.equippable_positions_occupied[position]:
                 self.wearing[position] = item
-                self.wearable_positions_occupied[position] = True
+                self.equippable_positions_occupied[position] = True
                 # remove the item from the source
                 source.pop(source.index(item))
                 return True
@@ -71,11 +73,11 @@ class Player:
             raise ValueError('Wrong position name')
 
     def strip(self, destination: list, position: str) -> bool:
-        if position in self.wearable_positions_occupied:
-            if self.wearable_positions_occupied[position]:
+        if position in self.equippable_positions_occupied:
+            if self.equippable_positions_occupied[position]:
                 item = self.wearing[position]
                 self.wearing.pop(position)
-                self.wearable_positions_occupied[position] = False
+                self.equippable_positions_occupied[position] = False
                 # add the stripped item to the destination list
                 destination.append(item)
                 return True
@@ -85,14 +87,14 @@ class Player:
             raise AssertionError('Wrong position name')
 
     def show_wearing(self) -> None:
-        for k in self.wearable_positions_occupied:
-            if self.wearable_positions_occupied[k]: # occupied position
+        for k in self.equippable_positions_occupied:
+            if self.equippable_positions_occupied[k]: # occupied position
                 print(f'{k}: {self.wearing[k].name}')
             else:
                 print(f'{k}: -')
 
     def show_inventory(self) -> None:
-        for i in self.items:
+        for i in self.inventory:
             print(i.name)
 
     def look(self):
@@ -104,15 +106,72 @@ class Player:
         else:
             print('There are no items in the room')
 
+    def equip_dialog(self) -> bool:
+        items_in_reach: list = []
+        for item in self.room.items:
+            if item.equippable:
+                items_in_reach.append([item, self.room.items])
+        for item in self.inventory:
+            if item.equippable:
+                items_in_reach.append([item, self.inventory])
+        if len(items_in_reach) > 0:
+            print('Which item do you want to equip?')
+            print('C/c or 0 for Cancel')
+            for i, item_source_pair in enumerate(items_in_reach):
+                print(f'{i + 1}: {item_source_pair[0].name}')
+            while True:
+                item_number = input('> ')
+                if item_number in [str(i + 1) for i in range(len(items_in_reach))]:
+                    item = items_in_reach[int(item_number) - 1][0]
+                    source = items_in_reach[int(item_number) - 1][1]
+                    break
+                elif item_number.lower() in ['c', '0']:
+                    return False
+                else:
+                    print('Wrong item number!')
+        else:
+            print('Nothing to equip.')
+            return False
+        free_positions = [
+            p for p in self.equippable_positions_occupied \
+            if self.equippable_positions_occupied[p] == False
+        ]
+        if len(free_positions) > 0 and item.equippable:
+            positions = list(set(item.equippable_positions).intersection(free_positions))
+            if len(positions) > 0:
+                if len(positions) == 1:
+                    print(f'Equipping {item.name} at {positions[0]}')
+                    self.equip(source = source, item = item, position = positions[0])
+                    return True
+                else:
+                    print('Equip at which position?')
+                    print('C/c or 0 for Cancel')
+                    for i, position in enumerate(positions):
+                        print(f'{i + 1}: {position}')
+                    while True:
+                        position_number = input('> ')
+                        if position_number in [str(i + 1) for i in range(len(positions))]:
+                            print(f'Equipping {item.name} at {positions[int(position_number) - 1]}')
+                            self.equip(source = source, item = item, position = positions[int(position_number) - 1])
+                            return True
+                        elif position_number.lower() in ['c', '0']:
+                            return False
+                        else:
+                            print('Wrong position number!')
+
+        else:
+            print('No free positions to equip anything')
+        return False
+
 
 def main():
     world = World()
     world.rooms.append(Room(id = 1, description = 'A small dark room'))
     p = Player(room = world.rooms[0])
-    p.items.append(Item(name = 'neco', weight = 2))
-    p.items.append(Clothes(name = 'shirt', wearable_positions = ['body'], weight = 2, color = 'red'))
-    p.items.append(Weapon(name = 'dagger', weight = 1, damage = 1))
-    p.room.items.append(Clothes(name = 'simple hat', wearable_positions = ['head'], weight = 1, color = 'gray'))
+    p.inventory.append(Item(name = 'neco', weight = 2))
+    p.inventory.append(Clothes(name = 'shirt', equippable_positions = ['body', 'legs'], weight = 2, color = 'red'))
+    p.inventory.append(Weapon(name = 'dagger', weight = 1, damage = 1))
+    p.room.items.append(Clothes(name = 'simple hat', equippable_positions = ['head'], weight = 1, color = 'gray'))
 
     commands = {
         'quit commands': ['q', 'quit', 'exit'],
@@ -138,21 +197,7 @@ def main():
         elif command in commands['look commands']:
             p.look()
         elif command in commands['equip commands']:
-            intems_in_reach: List[List[Item, list]] = []
-            for item in p.room.items + p.inventory:
-                if item.wearable:
-                    items_in_reach.append(item)
-            if len(items_in_reach) > 0:
-                print('Which item do you want to equip?')
-                for i, item in enumerate(intems_in_reach):
-                    print(f'{i + 1}: {item.name}')
-                while True:
-                    item_number = input('> ')
-                    if item_number in [str(i + 1) for i in range(len(items_in_reach))]:
-                        item = items_in_reach
-                        break
-                    else:
-                        print('Wrong item number!')
+            p.equip_dialog()
         else:
             print('Unknown command')
 
